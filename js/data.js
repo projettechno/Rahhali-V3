22e029dd83c45d3a531562cd949f11deb69bee02
Mouad097 <mrahhalisamlali@gmail.com>
2026-05-06 20:14:00 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -37,7 +37,7 @@ var tours = [
     {
     id: 3, title: "Ourika Valley Day Trip", subtitle: "Atlas Foothills Escape", category: "mountain",
     duration: "1 Day", price: "From \u20AC45per",
-    image: "images/ourika-main.jpg",
+    image: "images/ourika4.jpg",
     gallery: [
       "images/ourika-gallery-1.jpg",
       "images/ourika-gallery-2.jpg",

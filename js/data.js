d45890f146d246462f7289778f5613b6edc3d285
projettechno <95226779+projettechno@users.noreply.github.com>
2026-05-09 12:38:59 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -90,6 +90,7 @@ var tours = [
       "images/t1.jpg",
       "images/t2.jpg",
       "images/t3.jpg",
+      "https://www.youtube.com/embed/SPiZ9-85bXE"
       "images/t4.jpg",
       "images/t5.jpg",
       "images/t6.jpg",

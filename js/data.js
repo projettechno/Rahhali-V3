110e075e92a8ca6a5249663b4776f61e40f2650a
Mouad012300 <mariajossec20@gmail.com>
2026-05-16 14:57:00 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -60,6 +60,7 @@ itinerary: [
       "images/ourika-gallery-1.jpg",
       "images/ourika-gallery-2.jpg",
       "images/ourika-gallery-3.jpg",
+      "images/ourika6.jpeg",
       
     ],
     highlights: ["Discover the Berber village & tea with locals" , "Argan oil cooperative by Berber women", "Setti Fatma waterfalls visit", "Breathtaking Atlas Mountains landscape", "Visit Barbary House", "Mountain tour guide included"],
